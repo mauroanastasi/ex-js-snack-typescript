5ae7d0e9cf546dfc47f48ff140460110f85bde3f
mauroanastasi <mauroanastasi1@gmail.com>
2025-09-24 11:21:25 +0200

    [UPD] snack 2 done

--- src/main.ts
+++ src/main.ts
@@ -22,3 +22,21 @@ if (typeof dati === "string") {
   console.log("Tipo non supportato")
 }
 
+
+// Snack 2
+// Crea un type alias Dipendente che rappresenta un lavoratore con i seguenti dati:
+
+// nome → stringa
+// cognome → stringa
+// annoNascita → numero
+// sesso → Può essere solo "m" o "f".
+// anniDiServizio (array di numeri, es. [2014, 2015, 2017, 2018])
+
+type Dipendente = {
+  nome: string,
+  cognome: string,
+  annoNascita: number,
+  sesso: "m" | "f"
+  anniDiServizio: number[]
+}
+
