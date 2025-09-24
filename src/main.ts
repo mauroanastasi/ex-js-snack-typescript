eb3aebc789e70662995bffd8087fde31de20c4a4
mauroanastasi <mauroanastasi1@gmail.com>
2025-09-24 11:31:23 +0200

    [UPD] snack 2 done

--- src/main.ts
+++ src/main.ts
@@ -40,3 +40,24 @@ type Dipendente = {
   anniDiServizio: number[]
 }
 
+
+const user: Dipendente = {
+  nome: "Mauro",
+  cognome: "Ana",
+  annoNascita: 32,
+  sesso: "m",
+  anniDiServizio: [2016, 2017, 2018, 2019]
+}
+
+console.log(user)
+
+// Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
+
+// Developer
+// livelloEsperienza → Il livello di esperienza del developer (le scelte possibili sono solo “Junior”, “Mid” o “Senior”).
+// linguaggi → Un array contenente i linguaggi di programmazione utilizzati dal developer in azienda (opzionale, perché i neo assunti non hanno ancora dei linguaggi assegnati).
+// certificazioni → Un array di stringhe contenente certificazioni tecniche ottenute dal developer (può essere vuoto).
+// ProjectManager
+// teamSize → Il numero di persone nel team gestito dal Project Manager (può essere null se non ha ancora un team assegnato).
+// budgetGestito → Il totale del budget annuale gestito dal PM (opzionale).
+// stakeholderPrincipali → Un array di stringhe con i nomi dei principali stakeholder con cui il PM collabora (può essere vuoto).
